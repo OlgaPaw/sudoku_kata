103d2fa1384488151967ec2e0ecd0e57c850420b
OlgaPaw <olga.pawlowicz@gmail.com>
2020-01-30 13:14:54 +0100

    Add docstrings

--- src/sudoku.py
+++ src/sudoku.py
@@ -11,16 +11,28 @@ def sudoku(matrix):
 
 
 def solved(matrix: Matrix) -> bool:
+    """
+    Returns True if puzzle is solved, otherwise False.
+    """
     return False
 
 
 def missing(region: Iterable[int]) -> Set[int]:
+    """
+    Returns set of values missing in region (Iterable of 9 Integers).
+    """
     return set()
 
 
 def column(index: int, matrix: Matrix) -> Tuple:
+    """
+    Returns values in an index column in a matrix.
+    """
     return ()
 
 
 def square(row: int, column: int, matrix: Matrix) -> Tuple:
+    """
+    Returns values in a square delimited by row and column indexes.
+    """
     return ()
